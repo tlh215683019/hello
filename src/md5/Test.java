13b03c3796e1e3fcc10e7e32636bf051411ce5ed
tlh215683019 <215683019@qq.com>
2018-05-20 10:08:09 +0800

    First commit!

---
+++ src/md5/Test.java
@@ -0,0 +1,10 @@
+package md5;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+public class Test {
+
+	public static void main(String[] args) {
+	  System.out.println(DigestUtils.md5Hex("123"));
+	}
+}
